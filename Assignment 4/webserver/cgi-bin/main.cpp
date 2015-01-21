fd1f8622c2d5346489a8901aa5c078b5cddd15db
Lars Jaeqx <lars.jaeqx@gmail.com>
2015-01-21 23:06:18 +0100

    Read shm from gamepad process

---
+++ Assignment 4/webserver/cgi-bin/main.cpp
@@ -0,0 +1,34 @@
+#include <errno.h>
+#include <string>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <Semaphore.h>
+#include <SharedMemory.h>
+#include <mqueue.h>
+#include <iostream>
+
+using namespace std;
+
+Semaphore * sem;
+SharedMemory * shm;
+
+int main(int argc, char* argv[]) {
+	sem = new Semaphore("ShmSem");
+    sem->newSemaphore();
+    cout << "sem opened" << endl;
+
+    shm = new SharedMemory("Shm", 2048);
+    cout << "shm opened" << endl;
+	while(true)
+	{
+		cout << "calling semwait" << endl;
+	    sem->waitSemaphore();
+
+	    cout << shm->getData() << endl;
+
+	    cout << "calling sempost" << endl;
+	    sem->postSemaphore();
+	    usleep(1000);
+	}
+}
